5b3a545f6905ce8853ca1100c2a8cf0dbb399afe
Khalid Hossen <52601910+Khalid019@users.noreply.github.com>
2022-12-17 10:22:15 +0600

    how to create table!

---
+++ createTable.sql
@@ -0,0 +1,34 @@
+/* -------------------Lecture 02 ------------------- */
+/*create table employee (
+	employee_ID INT,
+    first_name VARCHAR(20),
+    last_name VARCHAR(20),
+    hourly_pay DECIMAL(5, 2),
+    hire_date DATE
+);
+*/
+/* rename table employee to workers */
+/* alter table employees
+add phone_number varchar(20); */
+
+/* select * from employees; */
+
+/* alter table employees
+rename column phone_number to email; */
+
+/* alter table employees
+modify email varchar(50); */
+
+/* alter table employees
+modify email varchar(50)
+after last_name;
+
+select * from employees;
+*/
+
+/* alter table employees
+drop column hire_date;
+
+select * from employees;
+*/
+
